38c1515e5f84022bf45afba46a6ca77934fffa38
Louesilv <59611944+Louesilv@users.noreply.github.com>
2020-02-29 15:05:14 +0100

    function async getHTML

--- server/test.js
+++ server/test.js
@@ -17,9 +17,27 @@ axios('https://www.maitresrestaurateurs.fr/annuaire/recherche', (error,response,
         console.log("test2");
       }
 });*/
+const option={
+    méthode: "get",
+    url: 'https://guide.michelin.com/fr/fr/ile-de-france/paris/restaurant/kokoro',
+    timeout:5000,
+};
+
+async function getHTML(options){
+    const response = await axios(options);
+    const {data, status,error} = response;
+    if (status >= 200 && status < 300) {
+        return console.log(data);
+    }
+    else{
+        console.log(error)  
+    }
+}
+getHTML(option);
+/*
 axios ( {
     méthode: "get",
     url: 'https://guide.michelin.com/fr/fr/ile-de-france/paris/restaurant/kokoro',
     timeout:5000,
   }).then (réponse => console.log (réponse.data))
-  .catch (erreur => console.log (erreur));
+  .catch (erreur => console.log (erreur));*/
